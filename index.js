0b8bc4636c6012e3383ea75455330168300c6fbb
Tanvir <zishantanvir05@gmail.com>
2025-03-24 22:29:44 +0600

    complete 65

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-    `mongodb+srv://bossUser:${process.env.DB_PASS}@cluster0.pmlso.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://bossUser:${process.env.DB_PASS}@cluster0.pmlso.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +23,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewCollection = client.db("bistroDB").collection("reviews");
+
+        app.get('/menu',async(req,res)=>{
+          const result = await menuCollection.find().toArray()
+          res.send(result)
+        })
+        app.get('/reviews',async(req,res)=>{
+          const result = await reviewCollection.find().toArray()
+          res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log(
@@ -31,7 +43,7 @@ async function run() {
         );
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
