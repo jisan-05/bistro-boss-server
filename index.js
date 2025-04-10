eee1377fb0adc8eceab0fe6577141722b24f830c
Tanvir <zishantanvir05@gmail.com>
2025-04-11 00:12:03 +0600

    complete 70

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -31,6 +31,7 @@ async function run() {
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
+        const paymentCollection = client.db("bistroDB").collection("payments");
 
         // jwt related api
         app.post("/jwt", async (req, res) => {
@@ -144,44 +145,43 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result);
         });
-        app.get('/menu/:id', async(req,res)=>{
+        app.get("/menu/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
-            const result = await menuCollection.findOne(query)
-            res.send(result)
-        })
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        });
 
-
-        app.post('/menu', verifyToken,verifyAdmin, async(req,res)=>{
+        app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
             const item = req.body;
-            const result = await menuCollection.insertOne(item)
-            res.send(result)
-        })
-        app.patch('/menu/:id',async(req,res)=>{
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        });
+        app.patch("/menu/:id", async (req, res) => {
             const item = req.body;
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set:{
-                    name:item.name,
-                    category:item.category,
-                    price:item.price,
-                    recipe:item.recipe,
-                    image:item.image
-                }
-            }  
-            const result = await menuCollection.updateOne(filter,updatedDoc)
-            res.send(result)
-        })
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image,
+                },
+            };
+            const result = await menuCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        });
 
-        app.delete('/menu/:id',verifyToken,verifyAdmin,async(req,res) =>{
+        app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) };
             console.log("Delete request received for ID:", id);
 
-            const result = await menuCollection.deleteOne(query)
+            const result = await menuCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
         app.get("/reviews", async (req, res) => {
             const result = await reviewCollection.find().toArray();
@@ -209,22 +209,45 @@ async function run() {
         });
 
         // payment intent
-        app.post('/create-payment-intent', async(req,res)=>{
-            const {price} = req.body;
-            const amount = parseInt(price * 100)
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
 
             const paymentIntent = await stripe.paymentIntents.create({
-                amount:amount,
-                currency:"usd",
-                payment_method_types:[
-                    "card"
-                ]
-            })
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
             res.send({
-                clientSecret: paymentIntent.client_secret
-            })
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
-        })
+        app.get("/payments/:email", verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if(req.params.email !== req.decoded.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            const result = await paymentCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/payments", async (req, res) => {
+            const payment = req.body;
+            const paymentResult = await paymentCollection.insertOne(payment);
+
+            // carefully delete each item from the cart
+            console.log("payment info", payment);
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map((id) => new ObjectId(id)),
+                },
+            };
+
+            const deleteResult = await cartCollection.deleteMany(query);
+
+            res.send({ paymentResult, deleteResult });
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
